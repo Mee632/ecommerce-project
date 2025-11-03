9f26c8af98636cca7cd500b137b46ff5fad99cd7
Jakob <84283522+Mee632@users.noreply.github.com>
2025-11-03 09:51:13 +0100

    init

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/mee632/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,12 @@
+package com.mee632.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+}
